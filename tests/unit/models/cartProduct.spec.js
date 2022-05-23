0f024ba05baa1937560ac53565c80521ad72ddc0
Ivan D'Uva <duva.ivan@outlook.com>
2022-05-22 22:47:30 -0300

    Agrega tests para metodos de aumentar y disminuir cantidad

--- tests/unit/models/cartProduct.spec.js
+++ tests/unit/models/cartProduct.spec.js
@@ -151,3 +151,110 @@ test('Obtener la cantidad de un producto en un carrito luego de borrar un item',
 
     expect(quantityUpdated).toBe(1);
 });
+
+test('Aumentar la cantidad de un producto inexistente en un carrito', async () => {
+    // Creamos el carrito
+    const cart = await CartModel.create();
+
+    const quantity = await CartProductModel.increaseQuantity(cart.id, 1);
+
+    expect(quantity).toBeNull();
+});
+
+test('Aumentar la cantidad de un producto en un carrito', async () => {
+    const productData = {
+        price: 50000.0,
+        type: ProductType.HOME,
+        name: 'Placard',
+    };
+
+    // Creamos el producto
+    const product = await ProductModel.create(productData);
+
+    // Creamos el carrito
+    const cart = await CartModel.create(product);
+
+    // Obtenemos la cantidad de items del producto
+    const quantity = await CartProductModel.getQuantity(cart.id, product.id);
+
+    expect(quantity).toBe(1);
+
+    // Aumentamos la cantidad de items en 1
+    const quantityUpdated = await CartProductModel.increaseQuantity(
+        cart.id,
+        product.id
+    );
+
+    expect(quantityUpdated).toBe(2);
+});
+
+test('Disminuir la cantidad de un producto inexistente en un carrito', async () => {
+    // Creamos el carrito
+    const cart = await CartModel.create();
+
+    const quantity = await CartProductModel.decreaseQuantity(cart.id, 1);
+
+    expect(quantity).toBeNull();
+});
+
+test('Disminuir la cantidad de un producto en un carrito', async () => {
+    const productData = {
+        price: 50000.0,
+        type: ProductType.HOME,
+        name: 'Placard',
+    };
+
+    // Creamos el producto
+    const product = await ProductModel.create(productData);
+
+    // Creamos el carrito
+    const cart = await CartModel.create(product);
+
+    // Obtenemos la cantidad de items del producto
+    const quantity = await CartProductModel.getQuantity(cart.id, product.id);
+
+    expect(quantity).toBe(1);
+
+    // Aumentamos la cantidad de items en 1
+    const quantityUpdated = await CartProductModel.increaseQuantity(
+        cart.id,
+        product.id
+    );
+
+    expect(quantityUpdated).toBe(2);
+
+    // Disminuimos la cantidad de items en 1
+    const quantityDecreased = await CartProductModel.decreaseQuantity(
+        cart.id,
+        product.id
+    );
+
+    expect(quantityDecreased).toBe(1);
+});
+
+test('Disminuir la cantidad de un producto en un carrito cuando habia un item', async () => {
+    const productData = {
+        price: 50000.0,
+        type: ProductType.HOME,
+        name: 'Placard',
+    };
+
+    // Creamos el producto
+    const product = await ProductModel.create(productData);
+
+    // Creamos el carrito
+    const cart = await CartModel.create(product);
+
+    // Obtenemos la cantidad de items del producto
+    const quantity = await CartProductModel.getQuantity(cart.id, product.id);
+
+    expect(quantity).toBe(1);
+
+    // Intentamos disminuir la cantidad en 1
+    const quantityUpdated = await CartProductModel.decreaseQuantity(
+        cart.id,
+        product.id
+    );
+
+    expect(quantityUpdated).toBeNull();
+});
