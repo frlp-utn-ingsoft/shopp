d2c0088be66d04d46bd741aa6066e61e355ddcb2
Rodrigo Jacznik <rodrigo.jacznik@501labs.com.ar>
2022-05-25 18:31:26 -0300

    Refactor para usar el createTable de models

--- fixtures/index.js
+++ fixtures/index.js
@@ -1,9 +1,11 @@
 const products = require('./products.json');
-const { Product } = require('../src/models/product.js');
+const Models = require('../src/models/index.js');
 
 async function insertIntoDB() {
-    await Product.sync({ force: true });
-    return Product.bulkCreate(products);
+    await Models.createTables(true);
+    await Models.CartModel.create();
+
+    return Models.ProductModel.Product.bulkCreate(products);
 }
 
 if (require.main === module) {
