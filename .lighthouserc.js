75c6a5bb7c3932abb83fb518821a06bbcda409c1
Ivan D'Uva <duva.ivan@outlook.com>
2022-05-26 11:33:44 -0300

    Arregla metricas de lighthouse

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -6,14 +6,11 @@ module.exports = {
             startServerCommand: 'npm run start',
         },
         assert: {
-            preset: 'lighthouse:recommended',
             assertions: {
-                'apple-touch-icon': 'off',
-                'csp-xss': 'off',
-                'installable-manifest': 'off',
-                'maskable-icon': 'off',
-                'service-worker': 'off',
-                'splash-screen': 'off',
+                'categories:performance': ['error', { minScore: 0.9 }],
+                'categories:accessibility': ['error', { minScore: 0.9 }],
+                'categories:best-practices': ['error', { minScore: 0.83 }],
+                'categories:seo': ['error', { minScore: 0.9 }],
             },
         },
     },
