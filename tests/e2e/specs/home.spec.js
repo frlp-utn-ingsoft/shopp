8d1bfa2af59e06c6117a157fe8115ea7a1988219
Noceloni <julian_noceloni@hotmail.com>
2022-06-05 18:27:44 -0300

    Agrego test para verificar existencia del footer

--- tests/e2e/specs/home.spec.js
+++ tests/e2e/specs/home.spec.js
@@ -44,4 +44,9 @@ describe('Home Test', () => {
             .should('contain.text', '5 %');
     });
 
+    it('Debería existir el footer con la información de contacto de Shopp', () => {
+        cy.visit('/');
+        cy.get('footer').should('be.visible');
+    });
+
 });
