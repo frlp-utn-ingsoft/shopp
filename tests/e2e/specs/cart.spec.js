ea3d05da0057024873ec2084138af726ff567c3b
Guillermo Del Vecchio <guillermo.deve@gmail.com>
2022-06-07 17:05:45 -0300

    feat(test): add cypress test

--- tests/e2e/specs/cart.spec.js
+++ tests/e2e/specs/cart.spec.js
@@ -9,6 +9,11 @@ describe('Cart', () => {
         cy.get('.product').should('have.length', 0);
     });
 
+    it('Deberia estar vacio y mostrar mensaje vacio', () => {
+        cy.visit('/cart');
+        cy.contains('No posee productos en el carrito');
+    });
+
     it('Deberia poder agregar un item al carrito', () => {
         cy.visit('/');
 
@@ -46,11 +51,15 @@ describe('Cart', () => {
 
     it('Deberia mostrar 2 productos con descuento en la pagina de descuento', () => {
         cy.visit('/discount');
-        cy.get('.product').should('have.length', 2)
-        cy.get('.product:nth-child(1) [data-testid="discount"]')
-            .should('have.text', '5 %')
+        cy.get('.product').should('have.length', 2);
+        cy.get('.product:nth-child(1) [data-testid="discount"]').should(
+            'have.text',
+            '5 %'
+        );
 
-        cy.get('.product:nth-child(2) [data-testid="discount"]')
-            .should('have.text', '10 %')
+        cy.get('.product:nth-child(2) [data-testid="discount"]').should(
+            'have.text',
+            '10 %'
+        );
     });
 });
