a53e596e7b9251b8bf299711fa99888cbcb4ef7d
juanpaladino15 <juanpaladino15@gmail.com>
2022-06-06 22:08:12 -0300

    feature(products):list products alphabetically

--- src/models/product.js
+++ src/models/product.js
@@ -55,6 +55,7 @@ const getAllProducts = (limit, skip, type) => {
             exclude: ['createdAt', 'updatedAt'],
         },
         where: where,
+        order: [['name', 'ASC']],
     });
 };
 
